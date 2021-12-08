d8fa3a5c4b3205d2f4835662b57a9c75f8a3e218
aurore-ry <aremy.dev@gmail.com>
2021-12-08 17:06:26 +0100

    is functionnal and secure now

--- src/index.php
+++ src/index.php
@@ -1,4 +1,10 @@
 <?php
-$a = "Its working!!!";
-echo $a;
-?>
+  require_once "./db.php";
+  $db = new MyPDO();
+  $stmt = $db->prepare('SELECT * FROM hiking');
+  $stmt->execute();
+
+  $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+  print_r($res)
+  
+  ?>
