5f2caf31899f17ce7e1b0a8d0088068f8c797097
aurore-ry <aremy.dev@gmail.com>
2021-12-15 03:54:00 +0100

    fix logout

--- src/includes/logout.inc.php
+++ src/includes/logout.inc.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-if (!isset($_SESSION["username"])) {
-  session_unset();
-  session_destroy();
-  header("location: /");
-  exit();
-}
+session_unset();
+session_destroy();
+header("location: /");
+exit();
+
